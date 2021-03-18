5686f4005de3c0e8a0f2ff408c048fea3127b89b
Vincenzo Pellegrini <vincenzo.pellegrini.2001@gmail.com>
2021-03-18 03:09:34 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dueGiorniDiSub',
+    version='0.0.1',
+    packages=['db', 'parseandsend'],
+    url='https://github.com/enzo-pellegrini/dueGiorniDiSub',
+    license='MIT',
+    author='Vincenzo Pellegrini',
+    author_email='vincenzo.pellegrini.2001@gmail.com',
+    description='A small telegram bot to send daily news on users\' RSS feeds and podcasts'
+)
