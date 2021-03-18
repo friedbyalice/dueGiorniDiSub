0c44c448e5b72a51eb6984f6b4692af98c8f46c1
Vincenzo Pellegrini <vincenzo.pellegrini.2001@gmail.com>
2021-03-18 03:22:01 +0100

    Same license

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=['db', 'parseandsend'],
     url='https://github.com/enzo-pellegrini/dueGiorniDiSub',
-    license='MIT',
+    license='GPLv3',
     author='Vincenzo Pellegrini',
     author_email='vincenzo.pellegrini.2001@gmail.com',
     description='A small telegram bot to send daily news on users\' RSS feeds and podcasts'
